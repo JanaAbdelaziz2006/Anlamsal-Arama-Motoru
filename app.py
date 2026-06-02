6930cc170df6955d16d101b2afa6a6d7b37c6a02
JanaAbdelaziz2006 <jana.m.t.aziz@gmail.com>
2026-06-02 17:42:00 +0200

    calistiriyorum

--- app.py
+++ app.py
@@ -510,3 +510,4 @@ st.caption("Powered by FAISS, Sentence-Transformers, and Streamlit")
 #ex: Derinlik Öncelikli Arama (DFS) sınır veri yapısı olarak ne kullanır?
 #ex: Komşuluk Listesi bellek olarak ne kadar yer kaplar?
 #ex: İkili Arama hangi paradigma ile çalışır?
+IDS nedir?
